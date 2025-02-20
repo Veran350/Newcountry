985331228dcc71f4c0faf16f8ab10ca90029bf49
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-20 07:17:18 +0100

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>NEWCOUNTRY - Construction Business</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <script src="/assets/js/script.js" defer></script>
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="/public/index.php">Home</a></li>
+            <li><a href="/public/about.php">About</a></li>
+            <li><a href="/public/contact.php">Contact</a></li>
+            <li><a href="/public/address.php">Address</a></li>
+            <li><a href="/public/admin/login.php">Admin</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
