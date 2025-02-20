68c5cc960f648aa0a3c68d8460c5ae4bddb3830d
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-20 07:21:07 +0100

    Create address.php

---
+++ public/address.php
@@ -0,0 +1,4 @@
+<?php include_once __DIR__ . '/../includes/header.php'; ?>
+<h1>Our Address</h1>
+<p>123 Construction Ave, City, Country.</p>
+<?php include_once __DIR__ . '/../includes/footer.php'; ?>
