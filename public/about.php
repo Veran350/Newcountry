a7ce1baf0edf38c428776e12629702c8ac4e37a8
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-02-20 18:36:01 +0100

    Delete public directory

--- public/about.php
+++
@@ -1,4 +0,0 @@
-<?php include_once __DIR__ . '/../includes/header.php'; ?>
-<h1>About NEWCOUNTRY</h1>
-<p>We are a construction business committed to building quality structures that last.</p>
-<?php include_once __DIR__ . '/../includes/footer.php'; ?>
--- public/address.php
+++
@@ -1,4 +0,0 @@
-<?php include_once __DIR__ . '/../includes/header.php'; ?>
-<h1>Our Address</h1>
-<p>123 Construction Ave, City, Country.</p>
-<?php include_once __DIR__ . '/../includes/footer.php'; ?>
